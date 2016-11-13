a3c08f9e15407b21b11b76ca9c1a9662d6ea1b46
Your name <jaocoelho93@gmail.com>
2016-11-13 17:32:24 +0000

    Added button that takes user to page with everyone's links. Cleaned up code a little bit in express.js server file to make it more readable

--- express_server.js
+++ express_server.js
@@ -10,12 +10,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cookieSession({
   name: 'session',
   keys: ["random key"],
-  maxAge: 24 * 60 * 60 * 1000 //
+  maxAge: 24 * 60 * 60 * 1000 // I copied this from the readme, didn't want to mess with it!
 }))
 app.set("view engine", "ejs");
 
 const usersDatabase = {};
 const urlDatabase = {};
+
 ///////////////////
 /// FUNCTIONS   ///
 ///////////////////
@@ -75,22 +76,81 @@ const generateId = function(lengthOfId){
 ////////////////////////////
 /// Begin Route Handling ///
 ////////////////////////////
-app.get("/urls", (req, res) => { //Home Page for URLs
+
+app.get("/u", (req, res) => { // Home page with everyone's links
+  res.render("urls_u", {
+    email: req.session.email,
+    urls: urlDatabase});
+});
+
+app.get("/", (req, res) => { //Simple redirect
+  if(req.cookies.email){
+  res.redirect("/urls");
+  }
+  else {
+    res.redirect("/u")
+  }
+});
+
+app.get("/register", (req, res) => { //Directs user to register form
+  if(req.session.email){
+    res.redirect("/");
+  }
+  res.render("urls_register", {email: req.session.email});
+});
+
+app.post("/register", (req, res) => { //Registers users and updates the database
+  if(checkEmail(req.body.email)){
+    res.status(400).send("Error Code 400. Email already exists");
+  }
+  else if(req.body.email && req.body.password){
+    const userId = generateId(6);
+    const hashedPass = bcrypt.hashSync(req.body.password, 10);
+    urlDatabase[req.body.email] = {};
+    usersDatabase[userId] = {id: userId, email: req.body.email, password: hashedPass}
+    req.session.email = req.body.email;
+    res.redirect("/urls");
+  }
+  else{
+    res.status(400).send("Error Code 404. Innapropriate request. Make sure that email and password are filled correctly.");
+  }
+});
+
+app.get("/login", (req, res) =>{  // Directs user to login form
+  if(req.session.email){
+    res.redirect("/");
+  }
+  res.render("urls_login", {email: req.session.email});
+});
+
+app.post("/login", (req, res) => {  //Authenticates passwords and email for login
+  let email = req.body.email;
+  let password = req.body.password;
+  if (checkEmail(email)){
+    if(bcrypt.compareSync(password, findPassword(email))){
+      req.session.email = email;
+      res.redirect("/urls")
+    }
+    else{
+      res.status(403).send("Error 403. Passwords do not match.");
+    }
+  }
+  else{
+      res.status(403).send("Error 403. Email does not exist in our records.");
+  }
+});
+
+app.get("/urls", (req, res) => { //Home Page for Logged in users. Displays their URLs
   let templateVars = {
     email: req.session.email,
     urls: urlDatabase
   };
-
   if (req.session.email){
   res.render("urls_index", templateVars);
   }
   else{
     res.redirect("/login");
   }
-});
-
-app.get("/", (req, res) => {
-  res.redirect("/urls");
 });
 
 app.get("/urls/new", (req, res) => { //Renders page where users create new links
@@ -102,7 +162,7 @@ app.get("/urls/new", (req, res) => { //Renders page where users create new links
   }
 });
 
-app.post("/urls", (req, res) => {     //Applies logic where new URL is created and indexed with long URL
+app.post("/urls", (req, res) => {     //Applies logic when new URL is created and indexed with long URL
   if(req.session.email){
   newShort = generateRandomString(6);
   urlDatabase[req.session.email][newShort] = req.body.longURL;
@@ -118,7 +178,9 @@ app.post("/urls/:id/delete", (req, res) => { //Handles request to delete a link
   delete urlDatabase[req.session.email][req.params.id];
   res.redirect("/urls");
   }
-
+  else {
+    res.send ("You cannot delete links without being logged in.")
+  }
 });
 
 app.get("/urls/:id/edit", (req, res) => {  //Renders page where users edit links
@@ -152,79 +214,19 @@ app.get("/urls/:shortURL", (req, res) => { // Redirects to Full websites using s
   res.send("Please login before attemting to access your short links.");
 });
 
-app.get("/login", (req, res) =>{  // Directs user to login form
-  if(req.session.email){
-    res.redirect("/");
-  }
-  res.render("urls_login", {email: req.session.email});
-
-
-});
-
-app.post("/login", (req, res) => {  //Authenticates passwords and email for login
-  let email = req.body.email;
-  let password = req.body.password;
-  if (checkEmail(email)){
-    if(bcrypt.compareSync(password, findPassword(email))){
-      req.session.email = email;
-      res.redirect("/urls")
-    }
-    else{
-      res.status(403).send("Error 403. Passwords do not match.");
-    }
+app.get("/u/:shortURL", (req, res) => { // Redirects to Full websites using short URL (Anyone can use)
+  let link = checkLink(req.params.shortURL)
+  if(link !== false){
+    res.redirect(link);
   }
   else{
-      res.status(403).send("Error 403. Email does not exist in our records.");
-  }
-});
-
-app.get("/register", (req, res) => { //Directs user to register form
-  if(req.session.email){
-    res.redirect("/");
-  }
-  res.render("urls_register", {email: req.session.email});
-});
-
-app.post("/register", (req, res) => { //Registers users and updates the database
-  if(checkEmail(req.body.email)){
-    res.status(400).send("Error Code 400. Email already exists");
-  }
-  else if(req.body.email && req.body.password){
-    const userId = generateId(6);
-    const hashedPass = bcrypt.hashSync(req.body.password, 10);
-    urlDatabase[req.body.email] = {};
-    usersDatabase[userId] = {id: userId, email: req.body.email, password: hashedPass}
-    req.session.email = req.body.email;
-    res.redirect("/urls");
-  }
-  else{
-    res.status(400).send("Error Code 404. Innapropriate request. Make sure that email and password are filled correctly.");
+    res.send("This short link does not exist.");
   }
 });
 
 app.post("/logout", (req, res) => { // Handles log out functionality by clearning cookies
   req.session = null;
   res.redirect("/urls");
-});
-
-app.get("/u", (req, res) => { // Renders page with everyone's links
-  res.render("urls_u", {
-    email: req.session.email,
-    urls: urlDatabase});
-});
-
-app.get("/u/:shortURL", (req, res) => { // Redirects to Full websites using short URL (Anyone can use)
-  let link = checkLink(req.params.shortURL)
-
-  if(link !== false){
-    res.redirect(link);
-  }
-  else{
-    res.send("This short link doe not exist.");
-  }
-
-
-
 });
 
 ////////////////////////////
@@ -236,7 +238,7 @@ app.listen(PORT, () => {
 });
 
 
- // for (var email in urlDatabase){
+
 
 
 
